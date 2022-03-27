41f00b09a458748058e2592c9ebc2218d4e69557
Donald Lemus <dlemuss4@miumg.edu.gt>
2022-03-27 17:24:27 -0600

    Agregar archivos de proyecto.

---
+++ Modificacion de la clase Carro/Clase/Carro.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificacion_de_la_clase_Carro.Clase
+{
+    internal class micarro
+    {
+        public string Marca {get;}
+        public string Color {get;}
+        public int MAXKPH {get;}
+
+        private int encendido = 0;
+        private int velocidad_actual = 0;
+
+        public micarro(string marca, string color, int maxkph)
+        {
+            Marca = marca;
+            Color = color;
+            MAXKPH = maxkph;
+            encendido = 0;
+            this.velocidad_actual = 0;
+        }
+        public string encerdermotor()
+        {
+            string mensaje = " ";
+            if (encendido == 0)
+            {
+                encendido = 1;
+                mensaje = "\nCarro encedido";
+                velocidad_actual = 0;
+            }
+            else
+            {
+               mensaje = "\nEl carro ya estaba encendido";
+            }
+            return mensaje;
+        }
+        public string acelerar()
+        {
+            if (encendido == 0)
+            {
+                return $"El carro está apagado";
+            }
+            string mensaje = "";
+            if(velocidad_actual < MAXKPH)
+            {
+                    velocidad_actual += 20;
+                    mensaje = $"Has acelerado 20 KPH";                
+            }
+            else
+            {
+                velocidad_actual = MAXKPH;
+                mensaje = "Vas a velocidad Maxima, no puedes acelerar mas";
+            }
+            return mensaje;
+        }
+
+        public string getvelocidad()
+        {
+            string mensaje = $"Vamos a {velocidad_actual} KPH\n";
+            return mensaje;
+        }
+
+        public string desacelerar()
+        {
+            string mensaje = " ";
+            if(velocidad_actual > 0)
+            {
+                velocidad_actual -= 15;
+                mensaje = $"Has desacelerado, vas a {velocidad_actual} KPH";
+            }
+            if(velocidad_actual < 0)
+            {
+                velocidad_actual = 0;
+                mensaje = $"\nLa velocidad es 0";
+            }
+            return mensaje;
+        }
+
+        public string frenar()
+        {
+            string mensaje = " ";
+            if(velocidad_actual > 0)
+            {
+                mensaje = $"\nEl carro ha frenado un poco";
+                velocidad_actual = velocidad_actual - 5;
+            }
+            return mensaje;
+        }
+
+        public string bocinar()
+        {
+            string mensaje = "\nBocina bip bip\n";
+            return mensaje;
+        }
+
+        public string apagarmotor()
+        {
+            string mensaje = " ";
+            if (velocidad_actual == 0)
+            {
+                mensaje = $"\nEl carro se ha apagado";
+            }
+            else
+            {
+                mensaje = $"\nEl carro ya esta apagado";
+            }
+            return mensaje;
+        }
+
+        public string frenartodo()
+        {
+            string mensaje = " ";
+            if(velocidad_actual == 0)
+            {
+                mensaje = "El carro se ha frenado por completo";
+            }
+            else
+            {
+                mensaje = "Disminuya mas su velocidad para frenar por completo\n";
+            }
+            return mensaje;
+        }
+
+        public string informacion()
+        {
+            return $"Carro: \nMarca: {Marca} Color: {Color} Velocidad Maxima: {MAXKPH}\n";
+        }
+
+    }
+}
---
+++ Modificacion de la clase Carro/Program.cs
@@ -0,0 +1,27 @@
+﻿using Modificacion_de_la_clase_Carro.Clase;
+
+micarro micarro = new micarro("Toyota", "Negro", 50);
+
+Console.WriteLine(micarro.informacion());
+Console.WriteLine(micarro.encerdermotor());
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine(micarro.acelerar());
+}
+Console.WriteLine(micarro.getvelocidad());
+
+Console.WriteLine(micarro.frenar());
+Console.WriteLine(micarro.getvelocidad());
+Console.WriteLine(micarro.bocinar());
+
+
+Console.WriteLine(micarro.frenartodo());
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(micarro.desacelerar());
+}
+Console.WriteLine(micarro.frenartodo());
+Console.WriteLine(micarro.apagarmotor());
